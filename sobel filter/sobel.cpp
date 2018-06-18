bf8f5f140de58b633b6f79dbeb1b2442ec6588d1
Vaibhav Setia <35863212+vaibhav-setia@users.noreply.github.com>
2018-06-18 16:31:51 +0530

    Create sobel.cpp
    Inbuilt implementation of sobel filter

---
+++ sobel filter/sobel.cpp
@@ -0,0 +1,57 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+/** @function main */
+int main( int argc, char** argv )
+{
+
+  Mat src, src_gray;
+  Mat grad;
+  char* window_name = "Sobel Demo - Simple Edge Detector";
+  int scale = 1;
+  int delta = 0;
+  int ddepth = CV_16S;
+
+  
+  /// Load an image
+  src = imread("sample.jpeg" , CV_LOAD_IMAGE_COLOR);
+
+  if( !src.data )
+  { return -1; }
+
+
+
+  /// Convert it to gray
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+
+  /// Create window
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+
+  /// Generate grad_x and grad_y
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
+
+  /// Gradient X
+  
+  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_x, abs_grad_x );
+
+  /// Gradient Y
+  
+  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_y, abs_grad_y );
+
+  /// Total Gradient (approximate)
+  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+
+ ///Show image
+  imshow( window_name, abs_grad_y);
+
+  waitKey(0);
+
+  return 0;
+  }
